2de2db9cf03639b233828e6833cffb4d97424b07
root <root@vm8748.lumadock.com>
2026-05-21 18:13:13 +0000

    Fix mood chart - reduce container height to 180

--- lib/widgets/weekly_mood_chart.dart
+++ lib/widgets/weekly_mood_chart.dart
@@ -71,9 +71,9 @@ class WeeklyMoodChart extends StatelessWidget {
     }
 
     return Container(
-      height: 200,
+      height: 180,
       padding: const EdgeInsets.all(16),
-      decoration: AppTheme.cardDecoration(
+      decoration: AppTheme.cardDecoration((
         color: Theme.of(context).brightness == Brightness.dark 
             ? AppTheme.darkCard 
             : Colors.white,
