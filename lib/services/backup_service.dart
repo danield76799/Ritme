c4c572e09815ee45a6d3867f7fd7eb656a2ce085
Daniel <danielplusly@gmail.com>
2026-05-28 07:11:50 +0000

    fix(backup): re-add share_plus import for shareBackup method

--- lib/services/backup_service.dart
+++ lib/services/backup_service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 import '../database/database_helper.dart';
 
 class BackupService {
